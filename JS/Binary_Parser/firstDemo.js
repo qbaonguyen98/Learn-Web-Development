1951a9c6fc783b9cbb96d6f344363fe3d80112a9
Quoc Bao Nguyen <reusnguyen@outlook.com>
2020-01-15 22:35:48 +0700

    update

--- JS/Binary_Parser/firstDemo.js
+++ JS/Binary_Parser/firstDemo.js
@@ -30,3 +30,20 @@ var buf = Buffer.from("450002c5939900002c06ef98adc24f6c850186d1", "hex");
 
 // Parse buffer and show result
 console.log(ipHeader.parse(buf));
+
+function Serializer() {
+    var Parser = require('binary-parser').Parser;
+    
+    this.parser = new Parser();
+    // this.builder = new Builder();
+}
+
+Serializer.start = function () {
+    return new Serializer();
+};
+
+var foo1 = Serializer.start();
+
+var foo2 = Serializer.start();
+
+console.log(foo1 === foo2);
