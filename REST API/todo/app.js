54323a91e905a80ce77d822286b93d785f3f659c
Quoc Bao Nguyen <qbaonguyen98@gmail.com>
2020-09-25 11:34:48 +0700

    update

--- REST API/todo/app.js
+++ REST API/todo/app.js
@@ -62,6 +62,67 @@ app.get('/api/v1/todos/:id', (req, res) => {
   });
 });
 
+// delete a todo
+app.delete('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  db.map((todo, index) => {
+    if (todo.id === id) {
+      db.splice(index, 1);
+      return res.status(200).send({
+        success: 'true',
+        message: 'Todo deleted successfuly',
+      });    
+    }
+  });
+  return res.status(404).send({
+    success: 'false',
+    message: 'todo not found',
+  });
+});
+
+// update a todo
+app.put('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  let todoFound;
+  let itemIndex;
+  db.map((todo, index) => {
+    if (todo.id === id) {
+      todoFound = todo;
+      itemIndex = index;
+    }
+  });
+  if (!todoFound) {
+    return res.status(404).send({
+      success: 'false',
+      message: 'todo not found',
+    });
+  }
+  if (!req.body.title) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required',
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required',
+    });
+  }
+  const updatedTodo = {
+    id: todoFound.id,
+    title: req.body.title || todoFound.title,
+    description: req.body.description || todoFound.description,
+  };
+  db.splice(itemIndex, 1, updatedTodo);
+
+  return res.status(201).send({
+    success: 'true',
+    message: 'todo updated successfully',
+    updatedTodo,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
 });
